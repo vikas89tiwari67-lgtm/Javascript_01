bbfb44eb065d6ff52c6764c361b03774881e9e31
vikas89tiwari67-lgtm <vikas89tiwari67@gmail.com>
2026-09-08 17:27:37 +0530

    scope and functions

--- scope.js
+++ scope.js
@@ -1,30 +1,48 @@
-// let a = 123 // global scope // it can be used anywhere in the code 
+// // let a = 123 // global scope // it can be used anywhere in the code 
 
-// if(true){
-//     //block scope
-//     let a = 798 // we can't use it out side this block
-//     console.log(a);
+// // if(true){
+// //     //block scope
+// //     let a = 798 // we can't use it out side this block
+// //     console.log(a);
     
-// }
-// for (let i  = 0; i  < 10; i ++) { // here i is only valide inside the loop we cann't use i out side the loop
-//     const element = i*i
-//     console.log(element)
+// // }
+// // for (let i  = 0; i  < 10; i ++) { // here i is only valide inside the loop we cann't use i out side the loop
+// //     const element = i*i
+// //     console.log(element)
     
-// }
+// // }
 
-// console.log(a);
+// // console.log(a);
 
 
 
-function one(){
-    let user = "abc123"
-    function two(){
-        let web = "abc.com"
-        console.log(user);
+// function one(){
+//     let user = "abc123"
+//     function two(){
+//         let web = "abc.com"
+//         console.log(user);
         
-    }
-    two()
+//     }
+//     // two()
     
 
+// }
+//  // one()
+
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++ intresting +++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++ two wayes to define a function
+console.log(addone(5))  
+function addone(num){ // if we define a function we can call this function even before creating this function 
+    return num+1
 }
-one()
+
+let addtwo = function(num){ // if we define a function in a variable then me can't call it before variabe is define 
+    return num+2
+}
+console.log(addtwo(5))//we alwayse have to call it after the function 
+
