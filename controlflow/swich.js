5cd6d4926f9aa0cec8ceb9d7048c82f5f3ed0309
vikas89tiwari67-lgtm <vikas89tiwari67@gmail.com>
2026-09-09 10:48:11 +0530

    switch

---
+++ controlflow/swich.js
@@ -0,0 +1,38 @@
+// sentex of swich
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+let month = 3
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("fab")
+        break;
+    case 3:
+        console.log("mar")
+        break;
+    case 4:
+        console.log("april")
+        break;
+
+    default:
+        console.log("default case excuted");
+        
+        break;
+}
+
+
+// important 
+
+//  after case we can campare a string also
