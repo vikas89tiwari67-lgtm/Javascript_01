24b41a2bb2b3e8ea195a111685812f139dfc3df5
vikas89tiwari67-lgtm <vikas89tiwari67@gmail.com>
2026-09-09 10:47:42 +0530

    if,else

---
+++ controlflow/conditionstatement.js
@@ -0,0 +1,40 @@
+// else, if, else if
+
+// let islogddin = true
+// if(islogddin){
+//     console.log("user loggrd in ");
+    
+// }
+
+
+
+// let temprature = 40
+// if (temprature >= 42){
+//     console.log("temprature is higer than 42")
+// } 
+// else if(temprature == 42){
+//     console.log("temprature == 42");
+
+// }
+// else{
+//     console.log("tempratre is less than 40");
+    
+// }
+
+
+
+// we can also write inliner if else statement 
+
+// let a=20
+// if (a==20) console.log(a);
+
+
+// scope of condition statement 
+
+// let  a  = 20
+// if (a>10){
+//     let b = 10
+//     console.log(b);
+    
+// }
+// console.log(b);//scope of b is on;y inside the if condition 
