b86dc6ac4ccb039ec950adc187ea7caef7dd4857
vikas89tiwari67-lgtm <vikas89tiwari67@gmail.com>
2026-09-10 14:46:08 +0530

    basic loops

---
+++ controlflow/for/whileloop.js
@@ -0,0 +1,57 @@
+//for 
+
+// for(let i = 0; i<=10; i++){
+//     console.log(i);
+    
+// }
+
+// for (let i  = 0; i  < 5; i ++) {
+//     console.log("its outerloop");
+    
+//     for (let j = 0; j < 5; j++) {
+//         console.log("its inner loop");
+//         ;
+        
+//     };
+    
+// }
+
+
+// break // it break the loop 
+// continue // it did not breal the loop but skip all operation after this keyword 
+
+
+
+
+// while
+
+// let i = 0
+// while(i<10){
+//     console.log(`value of index ${i}`);
+//     i+=2
+    
+// }
+
+
+// let arr = ["a", "b", "c"]
+// let i = 0
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++;
+    
+// }
+
+
+
+// do while
+
+// in do while loop firstly we run thee once then we chek the condition 
+
+let h = 11
+do {
+    console.log(`index is ${h}`);
+    h++
+    
+    
+} while (h<10);
+// like here h is 11 still loop will run once and print "index is 11", even we are breaking it on 10
