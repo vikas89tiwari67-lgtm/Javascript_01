24b7df80693696000c5a72b64f559ba92af785da
vikas89tiwari67-lgtm <vikas89tiwari67@gmail.com>
2026-09-10 15:16:50 +0530

    loop_on_array

---
+++ controlflow/for/loops_on_array.js
@@ -0,0 +1,42 @@
+// for of loop 
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+// for(const num of arr){
+//     console.log(`numbers are ${num}`);
+    
+// }
+
+// let str = "hello World"
+// for(const val of str){
+//     console.log(`each char of str is ${val}`)
+
+// }
+
+// we cannt use for of loop for object data type
+// so we i=use for in loop
+
+
+                                     // for in loop
+
+// let obj = {
+//     name : "abc",
+//     phone : 23456,
+//     status : "online"
+// }
+// for(const i in obj){
+//     console.log(i);
+    
+// }
+
+// for in loop give kes of any data type
+
+
+
+
+// for each loop
+
+
+let arr = [1,2,3,4,56,78,]
+arr.forEach(function(val){
+    console.log(val)    
+})
